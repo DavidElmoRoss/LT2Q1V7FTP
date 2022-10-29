101c5840c5332f32dd754b52070704885d84e9cf
David Elmo Ross <david.elmo.ross@icloud.com>
2022-10-29 13:30:59 -0400

    first commit

---
+++ LT2Q1V7FTP.cpp
@@ -0,0 +1,23 @@
+/*
+    When you run the program below it prints a 4 character message.
+    However, one of the characters is wrong and needs to be corrected. 
+    Identify the wrong character and what the character should be.            
+*/
+
+// Include Section
+#include "mbed.h"                   
+#include "DigitDisplay.h"          
+
+// Hardware definitions
+DigitDisplay bob(D2,D3);         
+int main(void)
+{
+    bob.clear();                
+    bob.setColon(0);            
+    bob.setBrightness(4);       
+    bob.writeRaw(0,0x71);    
+    bob.writeRaw(1,0x77);
+    bob.writeRaw(2,0x06);
+    bob.writeRaw(3,0x24);  
+    for(;;);
+}
